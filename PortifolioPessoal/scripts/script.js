689c356c705d81dd6f8b6d1562368a8e51da3b79
otaviog36 <74985404+otaviog36@users.noreply.github.com>
2026-05-08 18:39:34 +0000

    Agora tá legal

--- PortifolioPessoal/scripts/script.js
+++ PortifolioPessoal/scripts/script.js
@@ -1,23 +1,25 @@
 const botao = document.getElementById("modoClaroEscuro");
 console.log(botao);
 
-    let claro = true;
-    botao.addEventListener("click", function() {
-    if (claro) {
-        document.body.style.backgroundColor = "#343a40";
-        document.body.style.color = "white";
-        botao.textContent = "Modo Escuro";
-        botao.style.backgroundColor = "white";
-        botao.style.color = "black";
-        claro = true;
-    }else {
-        document.body.style.backgroundColor = "#e5e5e5";
-        document.body.style.color = "black";
-        botao.textContent = "Modo Claro";
-        botao.style.backgroundColor = "black";
-        botao.style.color = "white";
+        let claro = true;
+botao.addEventListener("click", function() {
+if (claro) {
+document.body.style.backgroundColor = "#212529";
+document.body.style.color = "#f8f9fa";
+botao.textContent = "Modo Escuro";
+botao.style.backgroundColor = "white";
+botao.style.color = "black";
 
-        claro = false;
-    }
-    claro = !claro;
+claro = true;
+}else {
+document.body.style.backgroundColor = "#e5e5e5";
+document.body.style.color = "#6a040f";
+botao.textContent = "Modo Claro";
+botao.style.backgroundColor = "black";
+botao.style.color = "white";
+
+claro = false;
+}
+claro = !claro;
 });
+document.body.style.color = "#6a040f";
